f6c2e3d0ea889b7754f2453a4ef5b266cef6ca53
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-11-18 13:44:59 -0500

    Now working locally

--- KlipTok.Dock/public/viewer.js
+++ KlipTok.Dock/public/viewer.js
@@ -11,6 +11,22 @@ twitch.onAuthorized(async function(auth) {
     // save our credentials
     token = auth.token;
     tuid = auth.userId;
+		var helixToken = auth.helixToken;
+
+		if (helixToken != null) {
+
+			var url = "https://api.twitch.tv/helix/users?id=" + auth.channelId;
+			var response = await fetch(url, {
+				headers: {
+					"Client-ID": "vqy8jhq1qgq3qx2v2j8e7hfk6zj9z0",
+					"Authorization": "Bearer " + helixToken
+				}
+			});
+
+			document.getElementById("logo").href = "https://kliptok.com/" + response.data[0].display_name;
+
+		}
+
 
 		twitch.rig.log("Twitch authenticated, calling localhost for channel: " + auth.channelId);
 
@@ -23,8 +39,17 @@ twitch.onAuthorized(async function(auth) {
 			 'Authorization': 'Bearer ' + token
 			}
 		});	
-		var text = await response.text();
-		twitch.rig.log(text);
+		var clippers = await response.json();
+		clippers = clippers.map(c => ({
+			...c, url: c.url || "icon.png"
+		}));
+
+		var template = "{{#.}}" +
+			"<li><img src='{{ url }}' /> <span>{{ name }}</span> {{ count }}</li>" +
+		"{{/.}}";
+		var rendered = Mustache.render(template, clippers);
+		document.getElementById('mostViewedClippers').innerHTML = rendered;
+
 	} catch (e) 
 	{
 		twitch.rig.log(e);
