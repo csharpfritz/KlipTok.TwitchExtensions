8255b1803df104852600d02a4347ab839c514e26
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2021-11-24 10:21:59 -0500

    Added build script

---
+++ KlipTok.Dock/build.cmd
@@ -0,0 +1,8 @@
+npm version %1
+git add package.json
+git commit -m "Updated and tagging version %1"
+git tag %1
+git push --tags
+docker build -t kliptok_extensionapi:%1 -t kliptok_extensionapi:latest -t ghcr.io/csharpfritz/kliptok_extensionapi:%1 -t ghcr.io/csharpfritz/kliptok_extensionapi:latest .
+docker push ghcr.io/csharpfritz/kliptok_extensionapi:%1
+docker push ghcr.io/csharpfritz/kliptok_extensionapi:latest
